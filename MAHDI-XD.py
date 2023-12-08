91668e9833e362aeab15b6890ab5fb6b065752a6
xd-mahdi <153292861+xd-mahdi@users.noreply.github.com>
2023-12-08 18:16:07 +0430

    Update MAHDI-XD.py

--- MAHDI-XD.py
+++ MAHDI-XD.py
@@ -5,5 +5,5 @@ os.system('git pull')
 os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
 os.system('pip uninstall requests')
 os.system ('pip install requests')
-os.system('xdg-open https://t.me/Afg_Black001')
+os.system('xdg-open https://www.facebook.com/profile.php?id=100085027835067')
 import xdma
